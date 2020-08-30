8a95398b1fd8968808f04b0691d9fb41d1e0c154
Ollie Jones <oathompsonjones@gmail.com>
2020-08-30 20:41:55 +0100

    Projects

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,8 +4,8 @@ import Colours from "../Colours.json";
 export default class Header extends Component {
     public render() {
         return (
-            <div style={{ backgroundColor: Colours.background[1], bottom: 0, position: "absolute", width: "100%", height: "auto" }}>
-                <p style={{ fontSize: 10, margin: 5, position: "relative" }}>
+            <div style={{ backgroundColor: Colours.background[1], bottom: 0, width: "100%", height: 15 }}>
+                <p style={{ fontSize: 10, margin: 5 }}>
                     Site created and maintained by Oliver Jones (oathompsonjones) since 2020.
                 </p>
             </div>
--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,19 +1,69 @@
 import React, { Component } from "react";
-import ProjectCard from "../Components/ProjectCard";
+import { Link } from "react-router-dom";
+import Card from "../Components/Card";
 import Astro from "../Images/Project Logos/Astro.png";
+import NodeJS from "../Images/Project Logos/NodeJS.png";
+import Interact from "../Images/Project Logos/Interact.png";
+import React1 from "../Images/Project Logos/React.png";
 
 export default class Projects extends Component {
     public render() {
         document.title = "Oliver Jones | Projects";
         return (
-            <div id="projects">
+            <div>
                 <ProjectCard heading="Astro" text={
                     <p>
-                        Astro is a multi-purpose Discord Bot.
-                        Astro is a project which I have been working on for a few years, and which I am still constantly updating. <br /> <br />
-                        For more information, go to < a href="https://astrodev.xyz" > astrodev.xyz</a> or join the <a href="https://discord.gg/yPjRzZe">Discord Server</a>.
+                        Astro is a multi-purpose Discord Bot which I have been working on for a few years, and which I am still constantly updating. <br />
+                        It is designed to be the only bot you could need in a Discord Server, and is made to be easy to operate for any user.<br />
+                        <br />
+                        For more information, go to <a href="https://astrodev.xyz">astrodev.xyz</a> or join the <a href="https://discord.gg/yPjRzZe">Discord Server</a>.
                     </p>
                 } image={Astro} />
+                <ProjectCard heading="Simple Node Utils" text={
+                    <p>
+                        This NPM package is designed to make programmers lives a little bit easier by providing a collection of useful functions.<br />
+                        These functions generally extend the prototypes of primitive objects in JavaScript, meaning you only need to import the package into your root file. <br />
+                        <br />
+                        For more information, go to <a href="https://github.com/oathompsonjones/simple-node-utils">github.com</a> or <a href="https://www.npmjs.com/package/simple-node-utils">npmjs.com</a>.
+                    </p>
+                } image={NodeJS} />
+                <ProjectCard heading="TBSHS Interact Website" text={
+                    <p>
+                        Interact is the the charity and fundraising organisation at <a href="https://tbshs.org">The Bishop's Stortford High School</a>.<br />
+                        I served as the President of Interact during 2020 and the beginning of 2021.<br />
+                        <br />
+                        Interact's website is located at <a href="https://tbshsinteract.live">tbshsinteract.live</a>.
+                    </p>
+                } image={Interact} />
+                <ProjectCard heading="This Website" text={
+                    <p>
+                        This website needs litte explanation. Read the <Link to="/">Home</Link> page to find out more.
+                    </p>
+                } image={React1} />
+            </div>
+        );
+    }
+}
+
+
+class ProjectCard extends Component {
+    constructor(public props: { heading: string; text: JSX.Element; image: string; }) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div >
+                <Card heading={this.props.heading} text={
+                    <div className="project">
+                        <img alt={this.props.heading} src={this.props.image} style={{
+                            float: "left",
+                            height: "100px",
+                            margin: 10
+                        }} />
+                        <p>{this.props.text}</p>
+                    </div>
+                } />
             </div>
         );
     }
