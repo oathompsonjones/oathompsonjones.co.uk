f0a645637117635f55764f8922867420f83b7ebd
Ollie Jones <oathompsonjones@gmail.com>
2020-11-21 16:17:54 +0000

    Updated contact page

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -6,23 +6,17 @@ export default class Social extends Component {
         document.title = "Oliver Jones | Contact";
         return (
             <div id="contact">
-                <Card heading="Contact Links" text={
-                    <div>
+                <Card heading="Social Links" text={
+                    <div id="contact-links" style={{ textAlign: "center" }}>
                         <p>
-                            I am available on many platforms, so feel free to contact me via any of those listed below. <br />
-                            <br />
+                            <a href="mailto:oathompsonjones@gmail.com">oathompsonjones@gmail.com</a><br />
+                            <a href="https://discord.com/users/310145094684639235">Discord</a><br />
+                            <a href="https://twitter.com/oathompsonjones">Twitter</a> <br />
+                            <a href="https://facebook.com/oathompsonjones">Facebook</a> <br />
+                            <a href="https://instagram.com/oathompsonjones">Instagram</a> <br />
+                            <a href="https://github.com/oathompsonjones">GitHub</a> <br />
+                            <a href="https://linkedin.com/in/oathompsonjones">LinkedIn</a> <br />
                         </p>
-                        <div id="contact-links" style={{ textAlign: "center" }}>
-                            <p>
-                                <a href="mailto:oathompsonjones@gmail.com">oathompsonjones@gmail.com</a><br />
-                                <a href="https://discord.com/users/310145094684639235">Discord</a><br />
-                                <a href="https://twitter.com/oathompsonjones">Twitter</a> <br />
-                                <a href="https://facebook.com/oathompsonjones">Facebook</a> <br />
-                                <a href="https://instagram.com/oathompsonjones">Instagram</a> <br />
-                                <a href="https://github.com/oathompsonjones">GitHub</a> <br />
-                                <a href="https://linkedin.com/in/oathompsonjones">LinkedIn</a> <br />
-                            </p>
-                        </div>
                     </div>
                 } />
             </div>
