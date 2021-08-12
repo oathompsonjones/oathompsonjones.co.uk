19a4370dbe6ae850c667c0073db14ab51fe0d3db
Ollie Jones <oathompsonjones@gmail.com>
2021-08-12 17:42:00 +0100

    Switch from css to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import Error from "./Pages/Error";
