f96c63ad46425503652d760a833aabd73838137e
Ollie Jones <oathompsonjones@gmail.com>
2022-01-24 14:59:39 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -77,8 +77,9 @@ void (async (): Promise<void> => {
     app.get("/twitter", (_req, res) => void res.redirect("https://twitter.com/oathompsonjones"));
 
     // Redirect http to https.
-    app.use((req, res) => {
+    app.use((req, res, next) => {
         if (req.protocol === "http") res.redirect(`https://${req.headers.host}${req.url}`);
+        next();
     });
     app.use(e.static(`${__dirname}/client/build`));
 
