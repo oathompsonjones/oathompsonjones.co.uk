020b4ad0dc6e84bf0e9ee7e94359df678f3cb47b
Ollie Jones <oathompsonjones@gmail.com>
2022-02-21 02:23:07 +0000

    Update Contact.tsx

--- client/src/Pages/Contact.tsx
+++ client/src/Pages/Contact.tsx
@@ -2,16 +2,28 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import React, { Component } from "react";
 import MobBr from "../Components/MobBr";
 
-export default class Social extends Component {
+export default class Social extends Component<{}, { email: string | null; firstName: string | null; lastName: string | null; message: string | null; subject: string | null; }> {
+    public constructor(props: {}) {
+        super(props);
+        this.state = { email: null, firstName: null, lastName: null, message: null, subject: null };
+    }
+
     public render(): JSX.Element {
         document.title = "Oliver Jones | Contact";
+
         return (
             <Container>
                 <Row>
                     <Col>
+                        <a href="https://twitter.com/oathompsonjones?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @oathompsonjones</a>
+                        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                         <Card bg="dark">
                             <Card.Header>
-                                <Card.Title>Contact Information</Card.Title>
+                                <Card.Title>Quick Links</Card.Title>
                             </Card.Header>
                             <Card.Body style={{ textAlign: "center" }}>
                                 <Card.Link href="/email">oathompsonjones@gmail.com</Card.Link><MobBr/>
