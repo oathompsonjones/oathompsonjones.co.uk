c278ab51c2d00e3b110498f288de00b68caefe2a
Ollie Jones <oathompsonjones@gmail.com>
2022-10-01 12:05:07 +0100

    Update Header.tsx

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,5 +1,4 @@
-import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import { AppBar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import { DarkMode as DarkModeIcon, LightMode as LightModeIcon, Menu as MenuIcon } from "@mui/icons-material";
 import React, { Component, MouseEvent } from "react";
 
@@ -28,78 +27,70 @@ export default class Header extends Component<{ theme: "dark" | "light"; toggleT
         };
 
         return (
-            <header>
-                <AppBar position="sticky" enableColorOnDark>
-                    <Container maxWidth="xl">
-                        <Toolbar variant="dense" disableGutters>
-                            {/* Contains the nav bar for mobile devices. */}
-                            <Box sx={{ display: { md: "none", xs: "flex" }, flexGrow: 1 }}>
-                                {/* Displays the menu icon to access the dropdown nav menu. */}
-                                <IconButton size="large" onClick={handleOpenNavMenu} color="inherit" >
-                                    <MenuIcon />
-                                </IconButton>
-                                {/* Contains the dropdown nav menu. */}
-                                <Menu id="menu-appbar"
-                                    anchorEl={this.state.anchorElNav}
-                                    anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
-                                    keepMounted
-                                    transformOrigin={{ horizontal: "left", vertical: "top" }}
-                                    open={Boolean(this.state.anchorElNav)}
-                                    onClose={handleCloseNavMenu}
-                                    sx={{ display: { md: "none", xs: "block" } }}
-                                >
-                                    {/* Renders a link to each page. */}
-                                    {pages.map((page) =>
-                                        <MenuItem component="a" href={page.link} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center">{page.label}</Typography>
-                                        </MenuItem>
-                                    )}
-                                </Menu>
-                            </Box>
-                            {/* Displays the main page title for the mobile nav bar. */}
-                            <Typography variant="h5" noWrap component="a" href="" sx={{
-                                color: "inherit",
-                                display: { md: "none", xs: "flex" },
-                                flexGrow: 1,
-                                fontFamily: "monospace",
-                                fontWeight: 700,
-                                letterSpacing: ".3rem",
-                                mr: 2,
-                                textDecoration: "none"
-                            }}>OATHOMPSONJONES</Typography>
-
-                            {/* Displays the main page title for the full screen nav bar. */}
-                            <Typography variant="h6" noWrap component="a" href="/" sx={{
-                                color: "inherit",
-                                display: { md: "flex", xs: "none" },
-                                fontFamily: "monospace",
-                                fontWeight: 700,
-                                letterSpacing: ".3rem",
-                                mr: 2,
-                                textDecoration: "none"
-                            }}>OATHOMPSONJONES</Typography>
-                            {/* Contains the full screen nav bar. */}
-                            <Box sx={{ display: { md: "flex", xs: "none" }, flexGrow: 1 }}>
+            <AppBar position="sticky" sx={{ marginBottom: "1%" }} enableColorOnDark>
+                <Container maxWidth="xl">
+                    <Toolbar variant="dense" disableGutters>
+                        {/* Contains the nav bar for mobile devices. */}
+                        <Box sx={{ display: { md: "none", xs: "flex" }, flexGrow: 1 }}>
+                            {/* Displays the menu icon to access the dropdown nav menu. */}
+                            <IconButton size="large" onClick={handleOpenNavMenu} color="inherit" >
+                                <MenuIcon />
+                            </IconButton>
+                            {/* Contains the dropdown nav menu. */}
+                            <Menu id="menu-appbar"
+                                anchorEl={this.state.anchorElNav}
+                                anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+                                keepMounted
+                                transformOrigin={{ horizontal: "left", vertical: "top" }}
+                                open={Boolean(this.state.anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{ display: { md: "none", xs: "block" } }}
+                            >
                                 {/* Renders a link to each page. */}
                                 {pages.map((page) =>
-                                    <MenuItem component="a" href={page.link} onClick={handleCloseNavMenu} sx={{ color: "white", display: "block", my: 2 }}>
-                                        {page.label}
+                                    <MenuItem component="a" href={page.link} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page.label}</Typography>
                                     </MenuItem>
                                 )}
-                            </Box>
-
-                            {/* Buttons to control dark/light theme. */}
-                            <IconButton edge="end" style={{ float: "right" }} sx={{ mr: 2 }} color="inherit" onClick={(): void => void this.props.toggleTheme()}>
-                                {this.props.theme === "dark"
-                                    ? <DarkModeIcon />
-                                    : <LightModeIcon />
-                                }
-                            </IconButton>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-                <br/>
-            </header>
+                            </Menu>
+                        </Box>
+                        {/* Displays the main page title for the mobile nav bar. */}
+                        <Typography variant="h5" noWrap component="a" href="" sx={{
+                            color: "inherit",
+                            display: { md: "none", xs: "flex" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            mr: 2,
+                            textDecoration: "none"
+                        }}>OATHOMPSONJONES</Typography>
+                        {/* Displays the main page title for the full screen nav bar. */}
+                        <Typography variant="h6" noWrap component="a" href="/" sx={{
+                            color: "inherit",
+                            display: { md: "flex", xs: "none" },
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            mr: 2,
+                            textDecoration: "none"
+                        }}>OATHOMPSONJONES</Typography>
+                        {/* Contains the full screen nav bar. */}
+                        <Box sx={{ display: { md: "flex", xs: "none" }, flexGrow: 1 }}>
+                            {/* Renders a link to each page. */}
+                            {pages.map((page) =>
+                                <MenuItem component="a" href={page.link} onClick={handleCloseNavMenu} sx={{ color: "white", display: "block", my: 2 }}>
+                                    {page.label}
+                                </MenuItem>
+                            )}
+                        </Box>
+                        {/* Buttons to control dark/light theme. */}
+                        <IconButton edge="end" style={{ float: "right" }} sx={{ mr: 2 }} color="inherit" onClick={(): void => void this.props.toggleTheme()}>
+                            {this.props.theme === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
+                        </IconButton>
+                    </Toolbar>
+                </Container>
+            </AppBar>
         );
     }
 }
