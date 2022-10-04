c8cb954d246b39279747211673a623c8bd35fece
Ollie Jones <oathompsonjones@gmail.com>
2022-10-04 15:11:08 +0100

    Update Footer.tsx

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -8,7 +8,13 @@ export class Footer extends Component<{ footerHeight: string; }> {
 
     public render(): JSX.Element {
         return (
-            <footer style={{ bottom: 0, height: this.props.footerHeight, margin: "0% 1%", position: "absolute" }}>
+            <footer style={{
+                bottom: 0,
+                height: this.props.footerHeight,
+                padding: "0% 1%",
+                position: "absolute",
+                width: "100%"
+            }}>
                 <Typography component="p" variant="caption">Website created by <a href="https://oathompsonjones.co.uk">Oliver Jones</a>.</Typography>
             </footer>
         );
