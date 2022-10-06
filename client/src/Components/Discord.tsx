15215d31c49d9b75ca44e141ddceaa4b9a024834
Ollie Jones <oathompsonjones@gmail.com>
2022-10-06 13:05:00 +0100

    Update Discord.tsx

--- client/src/Components/Discord.tsx
+++ client/src/Components/Discord.tsx
@@ -2,8 +2,6 @@ import { Component } from "react";
 
 export class Discord extends Component {
     public render(): JSX.Element {
-        document.title = "UOE Diplomacy Society | Contact";
-
         return (
             <span className="material-icons">discord</span>
         );
