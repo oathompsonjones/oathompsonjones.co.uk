11b70dee100001365f9a14208675fa7a3cb9f27f
Ollie Jones <oathompsonjones@gmail.com>
2022-10-09 00:45:59 +0100

    Update Footer.tsx

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -22,36 +22,37 @@ export class Footer extends Component<{ backgroundColour: string; borderColour: 
         ];
 
         return (
-            <Container component="footer" sx={{
+            <footer style={{
                 backgroundColor: this.props.backgroundColour,
                 borderTop: `5px solid ${this.props.borderColour}`,
                 bottom: 0,
-                displayPrint: "none",
                 maxHeight: this.props.footerHeight,
                 overflow: "hidden",
                 padding: "1%",
                 position: "absolute",
                 width: "100%"
             }}>
-                <Stack divider={<Divider sx={{ margin: "0.5%" }} />}>
-                    <Stack direction="row" alignItems="center">
-                        <Avatar src={Pfp} sx={{ margin: "1%" }} />
-                        <Stack flexGrow={1}>
-                            <Typography variant="h6">Oliver Andrew</Typography>
-                            <Typography variant="h6">Thompson Jones</Typography>
+                <Container sx={{ displayPrint: "none" }}>
+                    <Stack divider={<Divider sx={{ margin: "0.5%" }} />}>
+                        <Stack direction="row" alignItems="center">
+                            <Avatar src={Pfp} sx={{ margin: "1%" }} />
+                            <Stack flexGrow={1}>
+                                <Typography variant="h6">Oliver Andrew</Typography>
+                                <Typography variant="h6">Thompson Jones</Typography>
+                            </Stack>
+                            {socials.map(({ icon, link }, i) =>
+                                <Link key={i} color="inherit" href={link}>
+                                    {icon}
+                                </Link>
+                            )}
                         </Stack>
-                        {socials.map(({ icon, link }, i) =>
-                            <Link key={i} color="inherit" href={link}>
-                                {icon}
-                            </Link>
-                        )}
+                        <Stack direction="row" divider={<Divider orientation="vertical" sx={{ margin: "0% 1%" }} flexItem />}>
+                            <Typography variant="caption" align="right" flexGrow={1}>oathompsonjones</Typography>
+                            <Typography variant="caption">Website created by <a href="https://oathompsonjones.co.uk">Oliver Jones</a></Typography>
+                        </Stack>
                     </Stack>
-                    <Stack direction="row" divider={<Divider orientation="vertical" sx={{ margin: "0% 1%" }} flexItem />}>
-                        <Typography variant="caption" align="right" flexGrow={1}>oathompsonjones</Typography>
-                        <Typography variant="caption">Website created by <a href="https://oathompsonjones.co.uk">Oliver Jones</a></Typography>
-                    </Stack>
-                </Stack>
-            </Container>
+                </Container>
+            </footer>
         );
     }
 }
