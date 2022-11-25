99735f60e8e883ec5602dab5f785396c48a2c4e8
Ollie Jones <oathompsonjones@gmail.com>
2022-11-25 16:23:22 +0000

    Update UseLocalStorage.ts

--- client/src/Hooks/UseLocalStorage.ts
+++ client/src/Hooks/UseLocalStorage.ts
@@ -1,7 +1,5 @@
 import { useCallback, useState } from "react";
 
-// Type StringLike = string;
-
 export const useLocalStorage = <T extends string | null = string | null>(key: string, initialState: T): [T, (newValue: T) => void] => {
     const [value, setValue] = useState<T>(localStorage.getItem(key) as T | null ?? initialState);
     const updatedSetValue = useCallback((newValue: T) => {
