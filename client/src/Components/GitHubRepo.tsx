f489f6ac2e0eb242205c5fbf525ab94904399782
Ollie Jones <oathompsonjones@gmail.com>
2022-11-27 13:10:00 +0000

    Update GitHubRepo.tsx

--- client/src/Components/GitHubRepo.tsx
+++ client/src/Components/GitHubRepo.tsx
@@ -19,30 +19,24 @@ export const GitHubRepo = ({ index, repo }: { index: number; repo: GitHub.IRepo;
                     <Typography>Learn More</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    {
-                        repo.nameWithOwner.split("/")[0] !== "oathompsonjones"
-                            ? <>
-                                <Typography variant="h6">Team</Typography>
-                                <Typography>{repo.nameWithOwner.split("/")[0]}</Typography>
-                            </>
-                            : ""
-                    }
+                    {repo.nameWithOwner.split("/")[0] !== "oathompsonjones"
+                        ? <>
+                            <Typography variant="h6">Team</Typography>
+                            <Typography>{repo.nameWithOwner.split("/")[0]}</Typography>
+                        </>
+                        : ""}
                     <Typography variant="h6">Languages</Typography>
                     <Typography>{repoLanguages}</Typography>
                     <CardActions>
                         <Stack direction="row" justifyContent="space-evenly" alignItems="center" width="100%">
-                            {
-                                !repo.isPrivate
-                                    ? <Button size="small" href={repo.url}>View Code</Button>
-                                    : <Button size="small" disabled>View Code</Button>
-                            }
-                            {
-                                repo.homepageUrl !== null && repo.homepageUrl.length > 0
-                                    ? <Button size="small" href={repo.homepageUrl}>View Site</Button>
-                                    : repo.isPrivate
-                                        ? <Button size="small" disabled>View Site</Button>
-                                        : <Button size="small" href={`https://oathompsonjones.github.io/${repo.name}`}>View Site</Button>
-                            }
+                            {!repo.isPrivate
+                                ? <Button size="small" href={repo.url}>View Code</Button>
+                                : <Button size="small" disabled>View Code</Button>}
+                            {repo.homepageUrl !== null && repo.homepageUrl.length > 0
+                                ? <Button size="small" href={repo.homepageUrl}>View Site</Button>
+                                : repo.isPrivate
+                                    ? <Button size="small" disabled>View Site</Button>
+                                    : <Button size="small" href={`https://oathompsonjones.github.io/${repo.name}`}>View Site</Button>}
                         </Stack>
                     </CardActions>
                     {repo.description}
