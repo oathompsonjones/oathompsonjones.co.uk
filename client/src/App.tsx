9222f121de86b477ef80e140d42ccca67f67b4b8
Ollie Jones <oathompsonjones@gmail.com>
2022-12-03 04:03:15 +0000

    Error 500 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ export default (): JSX.Element => {
         { element: <Portfolio />, label: "Portfolio", link: "/portfolio" },
         { element: <Gallery />, label: "Gallery", link: "/gallery" },
         { element: <Contact />, label: "Contact Me", link: "/contact" },
-        { element: <Error />, label: "Error 404", link: "*" }
+        { element: <Error code={404} />, label: "Error 404", link: "*" }
     ];
 
     // Creates the page router.
@@ -25,12 +25,13 @@ export default (): JSX.Element => {
         createRoutesFromElements(
             // Creates a base root, passing the Root component as an element.
             // The root element takes an argument which lists the pages that need showing in the nav bar.
-            <Route path="/" element={<Root />}>{
+            <Route path="/" element={<Root />} errorElement={<Error code={500} />}>{
                 // Maps each page object into a Route element.
                 pages.map(({ element, label, link }) => <Route
                     index={link === "/"}
                     path={link === "/" ? undefined : link}
                     element={element}
+                    errorElement={<Error code={500} />}
                     loader={(): string => document.title = label.length > 0 ? `Oliver Jones | ${label}` : "Oliver Jones"}
                 />)
             }</Route>
--- client/src/Pages/Error.tsx
+++ client/src/Pages/Error.tsx
@@ -1,11 +1,17 @@
 import { Container, Typography } from "@mui/material";
 
 /**
- * Error 404.
+ * Handles any errors.
  *
- * @returns {JSX.Element} A 404 page.
+ * @param {({ code: 404 | 500; })} props An object containing the component props.
+ * @param {(404 | 500)} props.code The error code to display.
+ * @returns {JSX.Element} An error page.
  */
-export const Error = (): JSX.Element => <Container>
-    <Typography variant="h2" gutterBottom>Error 404 - Page not found.</Typography>
-    <Typography variant="subtitle1">These aren't the droids you're looking for.</Typography>
-</Container>;
+export const Error = ({ code }: { code: 404 | 500; }): JSX.Element => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const errorMessages = { 404: "Page not found.", 500: "Internal server error." };
+    return <Container>
+        <Typography variant="h2" gutterBottom>Error {code} - {errorMessages[code]}</Typography>
+        <Typography variant="subtitle1">These aren't the droids you're looking for. Click <a href="/">here</a> to go to the homepage.</Typography>
+    </Container>;
+};
