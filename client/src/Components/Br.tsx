f243ee54b8f0832160176a338fc9456c19414bdc
Ollie Jones <oathompsonjones@gmail.com>
2022-12-06 18:13:35 +0000

    Update Br.tsx

--- client/src/Components/Br.tsx
+++ client/src/Components/Br.tsx
@@ -5,7 +5,7 @@
  * @param {number} [props.lines] The number of line breaks to render. Defaults to 1.
  * @returns {JSX.Element} Multiple line breaks.
  */
-export const Br = ({ lines }: { lines: number; } = { lines: 1 }): JSX.Element => (
+export const Br = ({ lines }: { lines?: number; } = { lines: 1 }): JSX.Element => (
     <>
         {
             Array(lines).fill(0)
