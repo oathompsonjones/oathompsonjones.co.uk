187e053f0ae80ff6e7d61531781c42b1053b46c3
Ollie Jones <oathompsonjones@gmail.com>
2023-04-23 17:19:11 +0100

    Use gravatar for favicon too

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { AVATAR_URL_ } from "@/constants";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/contexts/themeContext";
 
@@ -6,9 +7,9 @@ import { ThemeProvider } from "@/contexts/themeContext";
 export const metadata: Metadata = {
     description: "Portfolio site for Oliver Jones (oathompsonjones).",
     icons: {
-        apple: "icons/apple-touch-icon.png",
-        icon: ["icons/favicon-32x32.png", "icons/favicon-16x16.png"],
-        shortcut: "icons/favicon.ico"
+        apple: AVATAR_URL_(180),
+        icon: [AVATAR_URL_(32), AVATAR_URL_(16)],
+        shortcut: AVATAR_URL_(16)
     },
     themeColor: "#1c7eea",
     title: "Oliver Jones",
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 import md5 from "md5";
 
-export const AVATAR_URL = `https://www.gravatar.com/avatar/${md5("oathompsonjones@gmail.com")}?s=2048`;
+export const AVATAR_URL_ = (n: number): string => `https://www.gravatar.com/avatar/${md5("oathompsonjones@gmail.com")}?s=${n}`;
+export const AVATAR_URL = AVATAR_URL_(2048);
