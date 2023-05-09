ebfc642bdddac0a487ac6bf208cafd73391a2bb9
Ollie Jones <oathompsonjones@gmail.com>
2023-05-09 01:12:24 +0100

    export as default

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,11 +1,11 @@
-import { useLocalStorage } from "./useLocalStorage";
+import useLocalStorage from "./useLocalStorage";
 
 /**
  * Uses the useLocalStorage hook to handle dark mode.
  *
  * @returns {[boolean, () => void]}
  */
-export function useDarkMode(): [boolean, () => void] {
+export default function useDarkMode(): [boolean, () => void] {
     const [isDarkMode, setDarkMode] = useLocalStorage<boolean>("isDarkMode", true);
     return [isDarkMode, (): void => setDarkMode(!isDarkMode)];
 }
