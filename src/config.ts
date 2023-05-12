3f5be14ad6091c372dad1000731db5d61286f88a
Ollie Jones <oathompsonjones@gmail.com>
2023-05-12 18:17:03 +0100

    Move config.json to project root

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
-import jsonImport from "./config.json";
+import jsonImport from "../config.json";
 
 type ConfigData = Readonly<typeof jsonImport>;
 type Config = ConfigData & { update: (obj: Partial<typeof config>) => void; };
 let config = jsonImport as Config;
 config.update = (obj: Partial<ConfigData>): void => {
     config = Object.assign(config, obj);
-    fs.writeFileSync("../../config.json", JSON.stringify(config, null, "\t"));
+    fs.writeFileSync("config.json", JSON.stringify(config, null, "\t"));
 };
 
 export default config;
