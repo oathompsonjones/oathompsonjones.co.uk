462d59b4ba3f298cb75ea9b74cd212f1b0462e56
Ollie Jones <oathompsonjones@gmail.com>
2023-05-16 00:34:34 +0100

    Remove responsiveFontSizes function (wasn't doing anything)

--- src/contexts/themeContext.tsx
+++ src/contexts/themeContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CssBaseline, ThemeProvider as MuiThemeProvider, createTheme, darken, lighten, responsiveFontSizes } from "@mui/material";
+import { CssBaseline, ThemeProvider as MuiThemeProvider, createTheme, darken, lighten } from "@mui/material";
 import { createContext, useContext, useMemo } from "react";
 import type { ReactNode } from "react";
 import type { Theme } from "@mui/material";
@@ -26,7 +26,7 @@ function ThemeProvider({ children }: { children: ReactNode; }): JSX.Element {
         light: "#ffffff",
         main: "#1c7eea"
     };
-    const theme: Theme = responsiveFontSizes(createTheme({
+    const theme: Theme = createTheme({
         palette: {
             background: {
                 default: isDarkMode ? colours.dark : colours.light,
@@ -37,7 +37,7 @@ function ThemeProvider({ children }: { children: ReactNode; }): JSX.Element {
         },
         // Makes any h tags render with the main site colour.
         typography: Object.fromEntries(["h1", "h2", "h3", "h4", "h5", "h6"].map((h) => [h, { color: colours.main }]))
-    }));
+    });
 
     return (
         <ThemeContext.Provider value={useMemo(() => ({ colours, theme, toggleTheme }), [isDarkMode])}>
