7b055ce38634a92a5a67b77599f083d8a3f9382b
Ollie Jones <oathompsonjones@gmail.com>
2023-05-19 11:29:15 +0100

    Update title.tsx

--- src/components/header/title.tsx
+++ src/components/header/title.tsx
@@ -13,7 +13,12 @@ export default function Title({ textColour }: { textColour: string; }): JSX.Elem
             color={textColour}
             component={Link}
             href="/"
-            sx={{ flexGrow: { md: 0, xs: 1 }, fontFamily: "monospace", fontWeight: 700, letterSpacing: ".3rem" }}
+            sx={{
+                flexGrow: { md: 0, xs: 1 },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem"
+            }}
             variant="h5"
         >
             OATHOMPSONJONES
