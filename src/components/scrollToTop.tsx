db6bbb0f09bb2527d1288a242eb2d6c37cc68ee9
Ollie Jones <oathompsonjones@gmail.com>
2023-05-27 20:57:21 +0100

    Make fixed

--- src/components/scrollToTop.tsx
+++ src/components/scrollToTop.tsx
@@ -5,19 +5,17 @@ import { useCallback } from "react";
 
 export default function ScrollToTop(): JSX.Element {
     const scrolling: boolean = useScrollTrigger({ disableHysteresis: true, threshold: 75 });
-    const scrollToTop = useCallback(() => {
-        window.scrollTo({ behavior: "smooth", top: 0 });
-    }, []);
+    const scrollToTop = useCallback(() => window.scrollTo({ behavior: "smooth", top: 0 }), []);
     return (
         <Zoom in={scrolling}>
             <Fab
                 onClick={scrollToTop}
                 size="small"
                 sx={{
-                    bottom: "2rem",
-                    left: "100%",
-                    position: "sticky",
-                    right: "2rem"
+                    bottom: 0,
+                    margin: "2rem",
+                    position: "fixed",
+                    right: 0
                 }}
             >
                 <KeyboardArrowUp />
