725a22d1a78d1e68bc4fa0d2be9746d7b92411da
Ollie Jones <oathompsonjones@gmail.com>
2023-06-02 11:41:20 +0100

    Improve background effect

--- src/app/(pages)/(home)/backgroundCursor.tsx
+++ src/app/(pages)/(home)/backgroundCursor.tsx
@@ -1,35 +1,35 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useThemeContext } from "contexts/themeContext";
 
 export default function BackgroundCursor(): JSX.Element {
-    const { theme: { palette: { primary: { main: colour } } } } = useThemeContext();
+    const { theme: { palette: { primary: { main } } } } = useThemeContext();
+    const [r, g, b] = main.slice(1).match(/.{2}/ug)!;
+    const colour = `rgba(${parseInt(r, 16)}, ${parseInt(g!, 16)}, ${parseInt(b!, 16)}, 0.2)`;
+    const [mouseX, setMouseX] = useState(0);
+    const [mouseY, setMouseY] = useState(0);
 
     useEffect(() => {
-        const effect = document.getElementById("cursor")!;
-        const size = effect.scrollHeight;
         document.addEventListener("mousemove", (e) => {
-            effect.style.opacity = "1";
-            effect.style.left = `${e.clientX}px`;
-            effect.style.top = `${e.clientY - size / 2}px`;
-            setTimeout(() => (effect.style.opacity = "0"), 1000);
+            setMouseX(e.clientX / innerWidth);
+            setMouseY(e.clientY / innerHeight);
         });
     }, []);
 
     return (
         <span
-            id="cursor"
             style={{
-                aspectRatio: "1 / 1",
-                background: `radial-gradient(${colour} -25%, transparent 50%)`,
-                borderRadius: "100%",
+                background: "transparent " +
+                    `radial-gradient(at calc(${mouseX} * 100%) calc(${mouseY} * 100%), ${colour}, transparent)` +
+                    " no-repeat 0 0",
                 height: "100%",
                 left: "50%",
-                opacity: 0,
+                objectFit: "cover",
+                overflow: "hidden",
                 position: "absolute",
-                top: "50%",
+                top: 0,
                 transform: "translateX(-50%)",
-                transition: "opacity 0.5s linear"
+                width: "100%"
             }}
         />
     );
