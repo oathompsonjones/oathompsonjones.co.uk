db228479ad389ff30487086d081fb0248360f273
Ollie Jones <oathompsonjones@gmail.com>
2023-10-02 15:44:16 +0100

    Improve privacy policy

--- src/app/(pages)/privacy/page.tsx
+++ src/app/(pages)/privacy/page.tsx
@@ -12,32 +12,13 @@ export default function Privacy(): JSX.Element {
             <Typography variant="h1">
                 Privacy Policy
             </Typography>
-            <Typography variant="h4">
-                What information do I collect?
-            </Typography>
             <Typography>
-                I may collect users names and email addresses.
-            </Typography>
-            <br />
-            <Typography variant="h4">
-                How do I collect the information?
-            </Typography>
-            <Typography>
-                Information is only shared with me if a user submits the form found <Link href="/contact">here</Link>.
-            </Typography>
-            <br />
-            <Typography variant="h4">
-                Why do I collect the information?
-            </Typography>
-            <Typography>
-                This information is collected to allow me to respond to any enquiries.
-            </Typography>
-            <br />
-            <Typography variant="h4">
-                Why do I share the information with?
-            </Typography>
-            <Typography>
-                This information is not shared with any other parties.
+                I reserve the right to collect the names and email address of any and all users who
+                submit the contact form located <Link href="/contact">here</Link>.
+                <br />
+                This information will not be shared with any other parties.
+                <br />
+                This information will only be used to allow me to respond to any enquiries.
             </Typography>
         </>
     );
