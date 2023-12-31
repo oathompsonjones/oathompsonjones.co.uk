8e48126a2c545bc397ee339bba028bb1a77db6a2
Ollie Jones <oathompsonjones@gmail.com>
2023-12-31 13:07:32 +0000

    Close db connection

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -11,7 +11,7 @@ export function register(): void {
     // Set up cron jobs.
     const cronJobs: Array<{ cronTime: string; func: (...args: never[]) => Promise<void> | void; }> = [
         { cronTime: "* * * * *", func: refreshToken },
-        { cronTime: "0 0 1 * *", func: async () => void await init() }
+        { cronTime: "0 0 1 * *", func: async () => (await init())[1]() }
     ];
     for (const { cronTime, func } of cronJobs) 
         void new CronJob(cronTime, func, null, true, "utc");
