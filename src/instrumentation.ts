080f00a4175c8df3968f5a442c898804342d58c6
Ollie Jones <oathompsonjones@gmail.com>
2023-12-31 13:09:41 +0000

    Fix startup log

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,15 @@
 import { CronJob } from "cron";
+import { LogLevel } from "api/logs";
 import axios from "axios";
 import { init } from "api/logs/init";
 import { refreshToken } from "api/instagram";
-import { useEffect } from "react";
 
-export function register(): void {
+export async function register(): Promise<void> {
     // Log the start up.
-    useEffect(() => void axios.post("/api/logs", "Server started."), []);
+    await axios.post("/api/logs", {
+        content: "Server started.",
+        level: LogLevel.INFO
+    });
 
     // Set up cron jobs.
     const cronJobs: Array<{ cronTime: string; func: (...args: never[]) => Promise<void> | void; }> = [
