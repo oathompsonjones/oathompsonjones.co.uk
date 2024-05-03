4f8faecb983843f9f798a2ee6e56f4804248b155
Ollie Jones <oathompsonjones@gmail.com>
2024-05-03 17:25:03 +0100

    Update CV

--- src/app/api/cv/index.ts
+++ src/app/api/cv/index.ts
@@ -1,4 +1,5 @@
 import cv from "assets/cv.json";
+import fs from "fs";
 
 const data = cv as CV;
 
@@ -14,72 +15,8 @@ export type CV = {
  * @returns The LaTeX document.
  */
 export function generateTex(content: string): string {
-    return "% Define the type of document.\n" +
-        "\\documentclass[10pt, a4paper]{article}\n" +
-        "\n" +
-        "% Set a more sensible document size.\n" +
-        "\\usepackage[margin=0.25in]{geometry}\n" +
-        "\n" +
-        "% Set the fonts.\n" +
-        "\\usepackage[default]{lato}\n" +
-        "\n" +
-        "% Prevent words from being split accross lines.\n" +
-        "\\tolerance=1\\emergencystretch=\\maxdimen\\hyphenpenalty=10000\\hbadness=10000\n" +
-        "\n" +
-        "% Prevent error from biber.\n" +
-        "\\usepackage{biblatex}\n" +
-        "\n" +
-        "% Set up hyperlinks.\n" +
-        "\\usepackage[hidelinks]{hyperref}\n" +
-        "\n" +
-        "% Allows the use of multiple columns.\n" +
-        "\\usepackage{vwcol}\n" +
-        "\n" +
-        "% Removes numbers from sections and the contents page.\n" +
-        "\\setcounter{secnumdepth}{0}\n" +
-        "\n" +
-        "% Remove page numbers.\n" +
-        "\\pagenumbering{gobble}\n" +
-        "\n" +
-        "% Styles the title.\n" +
-        "\\usepackage{xcolor}" +
-        "\\usepackage{titling}\n" +
-        "\t\\pretitle{\\centering\\vskip-2cm\\huge}\n" +
-        "\t\\posttitle{\\par}\n" +
-        "\t\\preauthor{\\centering}\n" +
-        "\t\\postauthor{\\par}\n" +
-        "\t\\predate{}\n" +
-        "\t\\postdate{}\n" +
-        "\t\\renewcommand\\maketitlehooka{\\par\\noindent{\\color{lightgray}\\hrulefill}\\par}\n" +
-        "\t\\renewcommand\\maketitlehookb{\\par\\noindent{\\color{lightgray}\\hrulefill}\\par}\n" +
-        "\n" +
-        "% Style section sections.\n" +
-        "\\usepackage[compact]{titlesec}\n" +
-        "\\titleformat{\\section}\n" +
-        "\t{\\color{black}\\normalfont\\large\\bfseries}{\\thesection}{0em}{}[{\\color{lightgray}\\titlerule\\titlerule}]\n" +
-        "\\titleformat{\\subsection}\n" +
-        "\t{\\color{black}\\normalfont}{\\thesubsection}{0em}{}[{\\color{lightgray}\\titlerule}]\n" +
-        "\n" +
-        "% Reduce line height in tables.\n" +
-        "\\renewcommand{\\arraystretch}{1}\n" +
-        "\n" +
-        "% Sets the title.\n" +
-        "\\title{Oliver Jones}\n" +
-        "\\author{\n" +
-        "\t\\href{mailto:oathompsonones@gmail.com}{oathompsonones@gmail.com}\n" +
-        "\t$\\bullet$\n" +
-        "\t07872 644095\n" +
-        "\t$\\bullet$\n" +
-        "\t\\href{https://oathompsonjones.co.uk}{oathompsonjones.co.uk}\n" +
-        "}\n" +
-        "\\date{}\n" +
-        "\n" +
-        "% Begin the document.\n" +
-        "\\begin{document}\n" +
-        "\\maketitle\n" +
-        "\\color{darkgray}" +
-        `${content}\n` +
-        "\\end{document}";
+    const skeleton = fs.readFileSync("src/assets/cv-skeleton.tex", "utf8");
+    return skeleton.replace("%CONTENT%", content);
 }
 
 /**
--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -6,6 +6,10 @@ import pdflatex from "node-pdflatex";
 
 const data = cv as CV;
 
+/**
+ * Gets the CV in PDF format.
+ * @returns The CV in PDF format.
+ */
 export async function GET(): Promise<NextResponse> {
     const tex = generateTex(Object.keys(data).map((section) => mapSection(section as keyof CV)).join("\n"));
     let pdf: Buffer | null = null;
