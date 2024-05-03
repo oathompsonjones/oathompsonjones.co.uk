9741cdb935d1b1423f276e4c8490aa0cb00647b0
Ollie Jones <oathompsonjones@gmail.com>
2024-05-04 00:44:19 +0100

    Add shadow behind image

--- src/app/(pages)/(home)/main.tsx
+++ src/app/(pages)/(home)/main.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Divider, Stack, Typography } from "@mui/material";
+import { Avatar, Button, Divider, Paper, Stack, Typography } from "@mui/material";
 import BackgroundImage from "./backgrounds/image";
 import { GRAVATAR_URL } from "utils";
 import Link from "next/link";
@@ -13,7 +13,7 @@ export default function Main(): ReactElement {
     return (
         <Section background={<BackgroundImage />}>
             <Stack alignItems="center" direction="column" justifyContent="center">
-                <Avatar src={GRAVATAR_URL} sx={{ height: "auto", width: { lg: "30%", md: "50%", sm: "70%", xs: "90%" } }} />
+                <Avatar src={GRAVATAR_URL} sx={{ height: "auto", width: { lg: "30%", md: "50%", sm: "70%", xs: "90%" } }} component={Paper} elevation={20} />
                 <Typography align="center" variant="h1">Oliver Jones</Typography>
                 <Typography align="center" variant="h3" color="secondary">BSc Computer Science Undergraduate</Typography>
                 <Typography align="center" variant="h4" color="secondary">The University of Edinburgh</Typography>
