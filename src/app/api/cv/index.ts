654dbad671447e9b00f54dd8c55c3cec04af833a
Ollie Jones <oathompsonjones@gmail.com>
2024-06-05 00:02:46 +0100

    De async function

--- src/app/api/cv/index.ts
+++ src/app/api/cv/index.ts
@@ -1,5 +1,5 @@
 import cv from "assets/cv.json";
-import fs from "fs/promises";
+import fs from "fs";
 
 const data = cv as CV;
 
@@ -14,8 +14,9 @@ export type CV = {
  * @param content - The content of the CV.
  * @returns The LaTeX document.
  */
-export async function generateTex(content: string): Promise<string> {
-    const skeleton = await fs.readFile("src/assets/cv-skeleton.tex", "utf8");
+export function generateTex(content: string): string {
+    // eslint-disable-next-line no-sync
+    const skeleton = fs.readFileSync("src/assets/cv-skeleton.tex", "utf8");
 
     return skeleton.replace("%CONTENT%", content);
 }
