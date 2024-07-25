577c6e631b653d6abf1e5011b827d5ff742a6aad
Ollie Jones <oathompsonjones@gmail.com>
2024-07-25 16:41:34 +0100

    Combine imageLinkOverlay and instagramPost

--- src/app/(pages)/gallery/instagramPost.tsx
+++ src/app/(pages)/gallery/instagramPost.tsx
@@ -1,8 +1,8 @@
-import ImageLinkOverlay from "components/imageLinkOverlay";
 import { Instagram } from "@mui/icons-material";
 import type { Post } from "api/instagram";
-import type { ReactElement } from "react";
-import { Zoom } from "@mui/material";
+import { useState, type ReactElement } from "react";
+import { Card, CardMedia, Zoom } from "@mui/material";
+import Link from "next/link";
 
 /**
  * Renders an Instagram post.
@@ -19,11 +19,13 @@ export default function InstagramPost({ post }: { readonly post: Post; }): React
         );
     }
 
-    // All other posts are displayed as a single image.
-    return (
-        // @ts-expect-error TypeScript configuration enforces that undefined properties and optional properties are different.
-        <ImageLinkOverlay effect={{ element: Zoom, props: { in: true } }} glow href={post.permalink} image={post.media_url}>
-            <Instagram sx={{
+    // The hover state is used to display the Instagram logo when the user hovers over the post.
+    const [hover, setHover] = useState(false);
+    const handleHover = (): void => setHover((prev) => !prev);
+
+    const instagramLogo = (
+        <Instagram
+            sx={{
                 color: "white",
                 cursor: "pointer",
                 height: "50%",
@@ -33,7 +35,51 @@ export default function InstagramPost({ post }: { readonly post: Post; }): React
                 transform: "translate(-50%, -50%)",
                 width: "50%",
             }}
-            />
-        </ImageLinkOverlay>
+        />
+    );
+
+    // A glow effect is displayed behind each post.
+    const glowEffect = (
+        <CardMedia
+            component="img"
+            image={post.media_url}
+            sx={{
+                filter: "blur(5px)",
+                height: "calc(100% + 0.25rem)",
+                margin: "-0.125rem",
+                position: "absolute",
+                width: "calc(100% + 0.25rem)",
+                zIndex: -1,
+            }}
+        />
+    );
+
+    // All other posts are displayed as a single image.
+    return (
+        <Zoom in timeout={500}>
+            <Card onMouseEnter={handleHover} onMouseLeave={handleHover} square
+                sx={{
+                    border: "none",
+                    boxShadow: "none",
+                    overflow: "visible",
+                    position: "relative",
+                }}
+            >
+                <Link
+                    href={post.permalink}
+                    style={{
+                        background: "rgba(0, 0, 0, 0.25)",
+                        display: hover ? "block" : "none",
+                        height: "100%",
+                        position: "absolute",
+                        width: "100%",
+                    }}
+                >
+                    {instagramLogo}
+                </Link>
+                {glowEffect}
+                <CardMedia component="img" image={post.media_url} sx={{ height: "100%", width: "100%" }} />
+            </Card>
+        </Zoom>
     );
 }
