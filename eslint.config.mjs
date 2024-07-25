124aea6ecf18fa050b2f5a703299d669b20ebf4b
Ollie Jones <oathompsonjones@gmail.com>
2024-07-25 16:42:44 +0100

    meta file updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import createConfig from "@oathompsonjones/eslint-config";
 
-export default createConfig("./tsconfig.json");
+export default [
+    ...createConfig("./tsconfig.json"),
+    {
+        rules: {
+            "require-unicode-regexp": "off",
+            "@next/next/no-html-link-for-pages": ["error", "src/app/(pages)"]
+        }
+    }
+];
