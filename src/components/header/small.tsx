2848cea1b808315f9c13902c404461bccd8071db
Ollie Jones <oathompsonjones@gmail.com>
2024-08-13 15:14:25 +0100

    Update small.tsx

--- src/components/header/small.tsx
+++ src/components/header/small.tsx
@@ -3,24 +3,28 @@ import Link from "next/link";
 import type { ReactElement } from "react";
 import useOutsideClick from "hooks/useOutsideClick";
 
-type IProps = {
-    readonly backgroundColor: string;
-    readonly toggleNavOpen: () => void;
-    readonly pages: Array<{ label: string; link: string; }>;
-};
-
 /**
  * Contains the nav bar for smaller displays.
  * @returns The small nav.
  */
-export default function SmallNav({ backgroundColor, toggleNavOpen, pages }: IProps): ReactElement {
+export default function SmallNav({ backgroundColor, open, pages, toggleNavOpen }: Readonly<{
+    backgroundColor: string;
+    open: boolean;
+    pages: Array<{ label: string; link: string; }>;
+    toggleNavOpen: () => void;
+}>): ReactElement {
     const ref = useOutsideClick(toggleNavOpen);
 
     return (
         <Box
             alignItems="center"
             ref={ref}
-            sx={{ backgroundColor, height: "100%", width: "100%" }}
+            sx={{
+                backgroundColor,
+                display: open ? "block" : "none",
+                height: "100%",
+                width: "100%"
+            }}
         >
             {pages.map((page, i) => (
                 <MenuItem
