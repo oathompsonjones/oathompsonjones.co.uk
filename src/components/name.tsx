a2ddf55c8380893ca5e4df556254e02c795a2ad7
Ollie Jones <oathompsonjones@gmail.com>
2024-08-14 15:47:57 +0100

    Update name.tsx

--- src/components/name.tsx
+++ src/components/name.tsx
@@ -15,14 +15,14 @@ export default function Name({ colour, id, minScreenSize, variant }: Readonly<{
 }>): ReactElement {
     return (
         <CollapsibleText
-            beginningText={colour ?? true ? [<><span className="colour">O</span>liver</>] : [<>Oliver</>]}
+            beginningText={colour ?? true ? [<><span className="colour">O</span>liver</>] : ["Oliver"]}
             collapsibleText={colour ?? true
                 ? [
                     <><span className="colour">A</span>ndrew</>,
                     <span className="colour" key={0}>Thompson</span>,
                 ]
-                : [<>Andrew</>, <>Thompson</>]}
-            endingText={colour ?? true ? [<span className="colour" key={0}>Jones</span>] : [<>Jones</>]}
+                : ["Andrew", "Thompson"]}
+            endingText={colour ?? true ? [<span className="colour" key={0}>Jones</span>] : ["Jones"]}
             id={`name-${id}`}
             minScreenSize={minScreenSize ?? "xs"}
             variant={variant ?? "body1"}
