bce2b65eee69b6ae61bcb801087d74a1886d2d70
Ollie Jones <oathompsonjones@gmail.com>
2024-08-14 12:58:42 +0100

    Neaten code

--- src/contexts/themeContext.tsx
+++ src/contexts/themeContext.tsx
@@ -30,11 +30,25 @@ export function ThemeProvider({ children }: { readonly children: ReactNode; }): 
     // Create the full theme.
     const theme: Theme = responsiveFontSizes(createTheme({
         components: {
-            MuiButton: { defaultProps: { variant: "contained" } },
-            MuiFab: { defaultProps: { color: "secondary" } },
+            MuiButton: {
+                defaultProps: {
+                    variant: "contained"
+                }
+            },
+            MuiFab: {
+                defaultProps: {
+                    color: "secondary"
+                }
+            },
             MuiPaper: {
-                defaultProps: { elevation: 5 },
-                styleOverrides: { root: { transition: "background-color 0.25s linear" } },
+                defaultProps: {
+                    elevation: 5
+                },
+                styleOverrides: {
+                    root: {
+                        transition: "background-color 0.25s linear"
+                    }
+                },
             },
         },
         palette: {
@@ -44,20 +58,20 @@ export function ThemeProvider({ children }: { readonly children: ReactNode; }): 
                 light: "#efefef",
             },
             mode: isDarkMode ? "dark" : "light",
-            primary: { main: "#1c7eea" },
-            secondary: { main: "#ea881c" },
+            primary: {
+                main: "#1c7eea"
+            },
+            secondary: {
+                main: "#ea881c"
+            },
         },
-        typography: (palette: Palette) => ({
-            caption: { color: palette.secondary.main },
-            h1: { color: palette.primary.main },
-            h2: { color: palette.primary.main },
-            h3: { color: palette.primary.main },
-            h4: { color: palette.primary.main },
-            h5: { color: palette.primary.main },
-            h6: { color: palette.primary.main },
-            subtitle1: { color: palette.secondary.main },
-            subtitle2: { color: palette.secondary.main },
-        }),
+        typography: (palette: Palette) => {
+            const primaryKeys = ["h1", "h2", "h3", "h4", "h5", "h6"];
+            const primaryObj = Object.fromEntries(primaryKeys.map((key) => [key, { color: palette.primary.main }]));
+            const secondaryKeys = ["caption", "subtitle1", "subtitle2"];
+            const secondaryObj = Object.fromEntries(secondaryKeys.map((key) => [key, { color: palette.secondary.main }]));
+            return { ...primaryObj, ...secondaryObj };
+        },
         zIndex: {
             appBar: 10,
             fab: 10,
@@ -71,11 +85,6 @@ export function ThemeProvider({ children }: { readonly children: ReactNode; }): 
                     <CssBaseline enableColorScheme />
                     <style>{/* CSS */`
                         :root {
-                            --breakpoint-xs: ${theme.breakpoints.values.xs}px;
-                            --breakpoint-sm: ${theme.breakpoints.values.sm}px;
-                            --breakpoint-md: ${theme.breakpoints.values.md}px;
-                            --breakpoint-lg: ${theme.breakpoints.values.lg}px;
-                            --breakpoint-xl: ${theme.breakpoints.values.xl}px;
                             --primary: ${theme.palette.primary.main};
                             --secondary: ${theme.palette.secondary.main};
                             --dark: ${theme.palette.background.dark};
