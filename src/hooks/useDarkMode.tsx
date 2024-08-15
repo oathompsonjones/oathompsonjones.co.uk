7b68300fc95c085055efd444315eaf3ca9f3a923
Ollie Jones <oathompsonjones@gmail.com>
2024-08-15 18:30:47 +0100

    Update useDarkMode.tsx

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,11 +1,16 @@
+import { useMediaQuery } from "@mui/material";
 import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
 
 /**
  * Uses the useLocalStorage hook to handle dark mode.
  * @returns Whether or not dark mode is enabled, and a function to toggle it.
  */
 export default function useDarkMode(): [boolean, () => void] {
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
     const [isDarkMode, setDarkMode] = useLocalStorage<boolean>("isDarkMode", true);
+
+    useEffect(() => setDarkMode(prefersDarkMode), [prefersDarkMode]);
 
     return [isDarkMode, (): void => setDarkMode(!isDarkMode)];
 }
