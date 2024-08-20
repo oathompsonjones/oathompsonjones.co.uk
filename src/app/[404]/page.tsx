6f6f2c92e890a6cc3e96a1ec8aab1f7b39c1bd03
Ollie Jones <oathompsonjones@gmail.com>
2024-08-20 17:12:25 +0100

    Page no longer needed

--- src/app/[404]/page.tsx
+++
@@ -1,8 +0,0 @@
-/*
-The not-found.tsx file is not rendered when clicking links which go nowhere, unless the user also then refreshes their page.
-This catches that issue.
-*/
-
-import Error from "app/not-found";
-
-export default Error;
