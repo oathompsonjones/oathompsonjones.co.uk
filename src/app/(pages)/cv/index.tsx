0b729bf2da2f2fc0e01c1528bf6439875656af22
Ollie Jones <oathompsonjones@gmail.com>
2024-09-07 21:56:29 +0100

    Update index.tsx

--- src/app/(pages)/cv/index.tsx
+++ src/app/(pages)/cv/index.tsx
@@ -7,20 +7,19 @@ import type { ReactNode } from "react";
  * @returns The mapped data.
  */
 export function jsonToJSDoc(content: string): ReactNode {
-    // eslint-disable-next-line prefer-named-capture-group
-    const matchingRegExp = /\[([^\]]+)\]\(([^\s)]+)\)/g;
-    const nonMatchingRegExp = /\[[^\]]+\]\([^\s)]+\)/g;
-    const links: string[] = content.match(matchingRegExp) ?? [];
-    const splitAtLinks: string[] = content.split(nonMatchingRegExp);
+    const markdownLink = /\[([^\]]+)\]\(([^\s)]+)\)/g;
+    const markdownLinkWithoutCapture = /\[[^\]]+\]\([^\s)]+\)/g;
+    const links: string[] = content.match(markdownLink) ?? [];
+    const splitAtLinks: string[] = content.split(markdownLinkWithoutCapture);
     const output: Array<ReactNode | string> = [];
 
     for (let i = 0, j = 0, k = 0; i < splitAtLinks.length || j < links.length; k++) {
         if (k % 2 === 0) {
             output.push(splitAtLinks[i++]);
         } else {
-            const link = links[j++];
-            const href = link!.replace(matchingRegExp, "$2");
-            const label = link!.replace(matchingRegExp, "$1");
+            const link = links[j++]!;
+            const href = link.replace(markdownLink, "$2");
+            const label = link.replace(markdownLink, "$1");
 
             output.push(<Link href={href} key={i}>{label}</Link>);
         }
