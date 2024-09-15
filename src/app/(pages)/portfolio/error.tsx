5dcaa3dd474fe33170614a5e98c3abdd276a7897
Ollie Jones <oathompsonjones@gmail.com>
2024-09-15 12:57:28 +0100

    Just export { PageError as default } instead of wrapping it

--- src/app/(pages)/portfolio/error.tsx
+++ src/app/(pages)/portfolio/error.tsx
@@ -1,15 +1,3 @@
 "use client";
 
-import { PageError } from "components/pageError";
-import type { ReactNode } from "react";
-
-/**
- * Handles errors for the page.
- * @param props - The component properties.
- * @param props.error - The error that occurred.
- * @param props.reset - The function to reset the application.
- * @returns An error element.
- */
-export default function Error({ error, reset }: { error: Error; reset: () => void; }): ReactNode {
-    return (<PageError error={error} reset={reset} />);
-}
+export { PageError as default } from "components/pageError";
