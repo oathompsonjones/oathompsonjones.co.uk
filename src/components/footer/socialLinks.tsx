ed07835ca797a8e9416b5d769aef811fa111bd44
Ollie Jones <oathompsonjones@gmail.com>
2024-09-15 12:56:59 +0100

    Remove dividers option as this is no longer be shown below the contact form due to it already being in the footer

--- src/components/footer/socialLinks.tsx
+++ src/components/footer/socialLinks.tsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, Divider, IconButton, Tooltip } from "@mui/material";
+import { ButtonGroup, IconButton, Tooltip } from "@mui/material";
 import { Email, Facebook, GitHub, Instagram, LinkedIn } from "@mui/icons-material";
 import { Discord } from "components/icons/Discord";
 import Link from "next/link";
@@ -8,11 +8,9 @@ import { TwitterX } from "components/icons/TwitterX";
 
 /**
  * Renders a list of social media links.
- * @param props - The properties of the component.
- * @param props.dividers - Whether to display dividers between the links.
  * @returns A list of social media links.
  */
-export function SocialLinks({ dividers = false }: { dividers?: boolean; }): ReactNode {
+export function SocialLinks(): ReactNode {
     // Links a URL and an icon for each social media to display.
     const socials: Array<{ icon: ReactNode; label: string; link: string; }> = [
         { icon: <Email />, label: "Email", link: "/email" },
@@ -28,21 +26,11 @@ export function SocialLinks({ dividers = false }: { dividers?: boolean; }): Reac
     return (
         <ButtonGroup sx={{ alignItems: "center", justifyContent: "space-evenly" }}>
             {socials.map(({ icon, label, link }, i) => (
-                <>
-                    {dividers && i > 0 && <Divider key={`divider-${i}`} orientation="vertical" />}
-                    <Tooltip key={`tooltip-${i}`} title={label} arrow>
-                        <IconButton
-                            component={Link}
-                            href={link}
-                            prefetch={false}
-                            sx={{ color: "inherit", padding: "var(--padding)" }}
-                            // eslint-disable-next-line @typescript-eslint/naming-convention
-                            style={{ "--padding": dividers ? "0.5rem" : "0" }}
-                        >
-                            {icon}
-                        </IconButton>
-                    </Tooltip>
-                </>
+                <Tooltip key={i} title={label} arrow>
+                    <IconButton component={Link} href={link} prefetch={false} sx={{ color: "inherit", padding: 0.5 }}>
+                        {icon}
+                    </IconButton>
+                </Tooltip>
             ))}
         </ButtonGroup>
     );
