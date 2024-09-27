b9e94e98bcacf463a4ce1bd58ca9ce9cf79d8e20
Ollie Jones <oathompsonjones@gmail.com>
2024-09-27 23:26:23 +0100

    Update proxy again

--- proxy.js
+++ proxy.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop, no-console */
 import fs from "fs/promises";
 import http from "http";
 import httpProxy from "http-proxy";
@@ -6,40 +6,47 @@ import https from "https";
 
 // Configuration.
 const baseDomain = "oathompsonjones.co.uk";
-const domainPortMap = new Map();
+const domainPortMap = {
+    [baseDomain]: { port: 3000, proxy: null },
+    [`haskell-graphics.${baseDomain}`]: { port: 3001, proxy: null },
+};
 
-domainPortMap.set(baseDomain, 3000);
-domainPortMap.set(`haskell-graphics.${baseDomain}`, 3001);
-
-// Setup proxy.
-const ssl = { cert: null, key: null };
-
-try {
-    ssl.key = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/privkey.pem`);
-    ssl.cert = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/fullchain.pem`);
-} catch (err) {
-    console.log(`Failed to read SSL certificates: ${err.message}`);
+// Setup proxies.
+for (const [domain, { port }] of domainPortMap) {
+    try {
+        domainPortMap.set(domain, {
+            port,
+            proxy: httpProxy.createProxy({
+                ssl: {
+                    cert: await fs.readFile(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
+                    key: await fs.readFile(`/etc/letsencrypt/live/${domain}/privkey.pem`),
+                },
+                target: `http://localhost:${port}`,
+            }),
+        });
+    } catch (err) {
+        console.log(`Failed to read SSL certificates: ${err.message}`);
+    }
 }
 
-const proxy = httpProxy.createProxy({ ssl });
-
 // Setup HTTPS server to proxy requests.
-const httpsServer = https.createServer(ssl, (req, res) => {
-    for (const [domain, port] of domainPortMap) {
-        if ((req.headers.host.startsWith("www.") ? req.headers.host.slice(4) : req.headers.host) === domain) {
-            proxy.web(req, res, { target: `http://localhost:${port}` });
-
-            return;
+const httpsServer = https.createServer((req, res) => {
+    if (req.headers.host !== undefined) {
+        for (const [domain, { proxy }] of domainPortMap) {
+            if ((req.headers.host.startsWith("www.") ? req.headers.host.slice(4) : req.headers.host) === domain)
+                return proxy.web(req, res);
         }
     }
 
-    res.end();
+    return res.end();
 });
 
 // Setup HTTP server to redirect to HTTPS.
 const httpServer = http.createServer((req, res) => {
-    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-    res.end();
+    if (req.headers.host !== undefined)
+        return res.writeHead(301, { Location: `https://${req.headers.host}${req.url ?? ""}` });
+
+    return res.end();
 });
 
 // Listen on ports 80 and 443.
