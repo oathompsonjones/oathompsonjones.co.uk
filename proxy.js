2e1de78b764260ddec5a535d5495f7262918ee3a
Ollie Jones <oathompsonjones@gmail.com>
2024-09-27 22:43:54 +0100

    Update proxy

--- proxy.js
+++ proxy.js
@@ -5,15 +5,18 @@ import httpProxy from "http-proxy";
 import https from "https";
 
 // Configuration.
-const domain = "oathompsonjones.co.uk";
-const port = 3000;
+const baseDomain = "oathompsonjones.co.uk";
+const domainPortMap = new Map();
+
+domainPortMap.set(baseDomain, 3000);
+domainPortMap.set(`haskell-graphics.${baseDomain}`, 3001);
 
 // Setup proxy.
 const ssl = { cert: null, key: null };
 
 try {
-    ssl.key = await fs.readFile(`/etc/letsencrypt/live/${domain}/privkey.pem`);
-    ssl.cert = await fs.readFile(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
+    ssl.key = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/privkey.pem`);
+    ssl.cert = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/fullchain.pem`);
 } catch (err) {
     console.log(`Failed to read SSL certificates: ${err.message}`);
 }
@@ -22,36 +25,31 @@ const proxy = httpProxy.createProxy({ ssl });
 
 // Setup HTTPS server to proxy requests.
 const httpsServer = https.createServer(ssl, (req, res) => {
-    if (req.headers.host !== domain) {
-        res.writeHead(404);
-        res.end();
+    for (const [domain, port] of domainPortMap) {
+        if ((req.headers.host.startsWith("www.") ? req.headers.post.slice(4) : req.headers.host) === domain) {
+            proxy.web(req, res, { target: `http://localhost:${port}` });
 
-        return;
+            return;
+        }
     }
 
-    proxy.web(req, res, { target: `http://localhost:${port}` });
+    res.writeHead(404);
+    res.end();
 });
 
 // Setup HTTP server to redirect to HTTPS.
 const httpServer = http.createServer((req, res) => {
-    if (req.headers.host !== domain) {
-        res.writeHead(404);
-        res.end();
-
-        return;
-    }
-
     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
     res.end();
 });
 
 // Listen on ports 80 and 443.
 try {
-    httpsServer.listen(443, domain);
+    httpsServer.listen(443);
     console.log("Listening on port 443");
 } catch (err) {
     console.log(`Failed to listen on port 443: ${err.message}`);
 } finally {
-    httpServer.listen(80, domain);
+    httpServer.listen(80);
     console.log("Listening on port 80");
 }
