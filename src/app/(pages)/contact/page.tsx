4419783c05d58c22b57719f33bb21c3405a188c5
Ollie Jones <oathompsonjones@gmail.com>
2024-10-07 00:11:48 +0100

    Add social links below contact form

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,17 +1,45 @@
+import { Button, Typography } from "@mui/material";
 import { ContactForm } from "components/pages/contact/contactForm";
+import Grid from "components/layout/grid";
+import type { GridSize } from "@mui/material";
+import Link from "next/link";
 import type { ReactNode } from "react";
-import { Typography } from "@mui/material";
+import { socials } from "components/footer/socialLinks";
 
 /**
  * This page provides contact links for me.
  * @returns My contact links, and an email form.
  */
 export default function Contact(): ReactNode {
+    const getSize = (size: "lg" | "md" | "sm" | "xl" | "xs", index: number): GridSize => {
+        const lineLength = { lg: 4, md: 3, sm: 2, xl: 5, xs: 1 }[size];
+
+        return index >= socials.length - socials.length % lineLength
+            ? "grow"
+            : socials.length / lineLength;
+    };
+
     // Renders the contact page.
     return (
         <>
             <Typography variant="h2">Contact Me</Typography>
             <ContactForm />
+            <Grid container spacing={1} columns={socials.length}>
+                {socials.map(({ icon, label, link }, i) => (
+                    <Grid key={i} size={(["xs", "sm", "md", "lg", "xl"] as const).map((size) => getSize(size, i))}>
+                        <Button
+                            LinkComponent={Link}
+                            href={link}
+                            startIcon={icon}
+                            color="inherit"
+                            variant="text"
+                            sx={{ display: "flex" }}
+                        >
+                            {label}
+                        </Button>
+                    </Grid>
+                ))}
+            </Grid>
         </>
     );
 }
--- src/components/footer/socialLinks.tsx
+++ src/components/footer/socialLinks.tsx
@@ -6,23 +6,23 @@ import type { ReactNode } from "react";
 import { StackOverflow } from "components/icons/StackOverflow";
 import { TwitterX } from "components/icons/TwitterX";
 
+// Links a URL and an icon for each social media to display.
+export const socials: Array<{ icon: ReactNode; label: string; link: string; }> = [
+    { icon: <Email />, label: "Email", link: "/email" },
+    { icon: <GitHub />, label: "GitHub", link: "/github" },
+    { icon: <StackOverflow />, label: "StackOverflow", link: "/stackoverflow" },
+    { icon: <LinkedIn />, label: "LinkedIn", link: "/linkedin" },
+    { icon: <Discord />, label: "Discord", link: "/discord" },
+    { icon: <TwitterX />, label: "Twitter/X", link: "/twitter" },
+    { icon: <Instagram />, label: "Instagram", link: "/instagram" },
+    { icon: <Facebook />, label: "Facebook", link: "/facebook" },
+];
+
 /**
  * Renders a list of social media links.
  * @returns A list of social media links.
  */
 export function SocialLinks(): ReactNode {
-    // Links a URL and an icon for each social media to display.
-    const socials: Array<{ icon: ReactNode; label: string; link: string; }> = [
-        { icon: <Email />, label: "Email", link: "/email" },
-        { icon: <GitHub />, label: "GitHub", link: "/github" },
-        { icon: <StackOverflow />, label: "StackOverflow", link: "/stackoverflow" },
-        { icon: <LinkedIn />, label: "LinkedIn", link: "/linkedin" },
-        { icon: <Discord />, label: "Discord", link: "/discord" },
-        { icon: <TwitterX />, label: "Twitter/X", link: "/twitter" },
-        { icon: <Instagram />, label: "Instagram", link: "/instagram" },
-        { icon: <Facebook />, label: "Facebook", link: "/facebook" },
-    ];
-
     return (
         <ButtonGroup sx={{ alignItems: "center", justifyContent: "space-evenly" }}>
             {socials.map(({ icon, label, link }, i) => (
