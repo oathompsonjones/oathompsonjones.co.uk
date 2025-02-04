52a850f56c3b6f9cc557ecb7c340df8272635f4d
Ollie Jones <oathompsonjones@gmail.com>
2025-02-04 16:11:44 +0000

    Change preivous commit as I can apparently use one certificate for multiple domains

--- proxy.js
+++ proxy.js
@@ -5,37 +5,30 @@ import httpProxy from "http-proxy";
 import https from "https";
 
 // Configuration.
+const sslPath = "/etc/letsencrypt/live/oathompsonjones";
 const domainPortMap = {
     "haskell-playground.co.uk": 3001,
     "oathompsonjones.co.uk": 3000,
 };
 
 // Setup proxy.
-const ssl = Object.keys(domainPortMap).reduce((acc, domain) => {
-    acc[domain] = { cert: null, key: null };
+const ssl = { cert: null, key: null };
 
-    return acc;
-}, {});
-
-for (const domain of Object.keys(domainPortMap)) {
-    try {
-        // eslint-disable-next-line no-await-in-loop
-        ssl[domain].cert = await fs.readFile(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
-        // eslint-disable-next-line no-await-in-loop
-        ssl[domain].key = await fs.readFile(`/etc/letsencrypt/live/${domain}/privkey.pem`);
-    } catch (err) {
-        console.log(`Failed to read SSL certificates for ${domain}: ${err.message}`);
-    }
+try {
+    ssl.cert = await fs.readFile(`${sslPath}/fullchain.pem`);
+    ssl.key = await fs.readFile(`${sslPath}/privkey.pem`);
+} catch (err) {
+    console.log(`Failed to read SSL certificates: ${err.message}`);
 }
 
-const proxy = httpProxy.createProxy();
+const proxy = httpProxy.createProxy({ ssl });
 
 // Setup HTTPS server to proxy requests.
-const httpsServer = https.createServer((req, res) => {
+const httpsServer = https.createServer(ssl, (req, res) => {
     if (req.headers.host !== undefined) {
         for (const [domain, port] of Object.entries(domainPortMap)) {
             if ((req.headers.host.startsWith("www.") ? req.headers.host.slice(4) : req.headers.host) === domain)
-                return proxy.web(req, res, { ssl: ssl[domain], target: `http://localhost:${port}` });
+                return proxy.web(req, res, { target: `http://localhost:${port}` });
         }
     }
 
