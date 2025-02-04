c533253a0131e2d2c42f048a0d1a0293d9a45633
Ollie Jones <oathompsonjones@gmail.com>
2025-02-04 16:05:03 +0000

    Update proxy for new diss project URL

--- proxy.js
+++ proxy.js
@@ -5,30 +5,37 @@ import httpProxy from "http-proxy";
 import https from "https";
 
 // Configuration.
-const baseDomain = "oathompsonjones.co.uk";
 const domainPortMap = {
-    [baseDomain]: 3000,
-    [`haskell-graphics.${baseDomain}`]: 3001,
+    "haskell-playground.co.uk": 3001,
+    "oathompsonjones.co.uk": 3000,
 };
 
 // Setup proxy.
-const ssl = { cert: null, key: null };
+const ssl = Object.keys(domainPortMap).reduce((acc, domain) => {
+    acc[domain] = { cert: null, key: null };
 
-try {
-    ssl.cert = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/fullchain.pem`);
-    ssl.key = await fs.readFile(`/etc/letsencrypt/live/${baseDomain}/privkey.pem`);
-} catch (err) {
-    console.log(`Failed to read SSL certificates: ${err.message}`);
+    return acc;
+}, {});
+
+for (const domain of Object.keys(domainPortMap)) {
+    try {
+        // eslint-disable-next-line no-await-in-loop
+        ssl[domain].cert = await fs.readFile(`/etc/letsencrypt/live/${domain}/fullchain.pem`);
+        // eslint-disable-next-line no-await-in-loop
+        ssl[domain].key = await fs.readFile(`/etc/letsencrypt/live/${domain}/privkey.pem`);
+    } catch (err) {
+        console.log(`Failed to read SSL certificates for ${domain}: ${err.message}`);
+    }
 }
 
-const proxy = httpProxy.createProxy({ ssl });
+const proxy = httpProxy.createProxy();
 
 // Setup HTTPS server to proxy requests.
-const httpsServer = https.createServer(ssl, (req, res) => {
+const httpsServer = https.createServer((req, res) => {
     if (req.headers.host !== undefined) {
         for (const [domain, port] of Object.entries(domainPortMap)) {
             if ((req.headers.host.startsWith("www.") ? req.headers.host.slice(4) : req.headers.host) === domain)
-                return proxy.web(req, res, { target: `http://localhost:${port}` });
+                return proxy.web(req, res, { ssl: ssl[domain], target: `http://localhost:${port}` });
         }
     }
 
