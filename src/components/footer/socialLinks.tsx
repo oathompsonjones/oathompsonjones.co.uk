7a048fb0709d3eef875e5cd2f7ddb875daf132d0
Ollie Jones <oathompsonjones@gmail.com>
2025-05-04 00:10:50 +0100

    Only display name in tooltip

--- src/components/footer/socialLinks.tsx
+++ src/components/footer/socialLinks.tsx
@@ -7,15 +7,15 @@ import { StackOverflow } from "components/icons/StackOverflow";
 import { TwitterX } from "components/icons/TwitterX";
 
 // Links a URL and an icon for each social media to display.
-export const socials: Array<{ icon: ReactNode; label: string; link: string; }> = [
-    { icon: <Email />, label: "Email: oathompsonjones@gmail.com", link: "/email" },
-    { icon: <GitHub />, label: "GitHub: @oathompsonjones", link: "/github" },
-    { icon: <StackOverflow />, label: "StackOverflow: @oathompsonjones", link: "/stackoverflow" },
-    { icon: <LinkedIn />, label: "LinkedIn: @oathompsonjones", link: "/linkedin" },
-    { icon: <Discord />, label: "Discord: @oathompsonjones", link: "/discord" },
-    { icon: <TwitterX />, label: "Twitter/X: @oathompsonjones", link: "/twitter" },
-    { icon: <Instagram />, label: "Instagram: @oathompsonjones", link: "/instagram" },
-    { icon: <Facebook />, label: "Facebook: Ollie Jones", link: "/facebook" },
+export const socials: Array<{ icon: ReactNode; label: string; link: string; name: string; }> = [
+    { icon: <Email />, label: "Email", link: "/email", name: "oathompsonjones@gmail.com" },
+    { icon: <GitHub />, label: "GitHub", link: "/github", name: "@oathompsonjones" },
+    { icon: <StackOverflow />, label: "StackOverflow", link: "/stackoverflow", name: "@oathompsonjones" },
+    { icon: <LinkedIn />, label: "LinkedIn", link: "/linkedin", name: "@oathompsonjones" },
+    { icon: <Discord />, label: "Discord", link: "/discord", name: "@oathompsonjones" },
+    { icon: <TwitterX />, label: "Twitter/X", link: "/twitter", name: "@oathompsonjones" },
+    { icon: <Instagram />, label: "Instagram", link: "/instagram", name: "@oathompsonjones" },
+    { icon: <Facebook />, label: "Facebook", link: "/facebook", name: "Ollie Jones" },
 ];
 
 /**
@@ -25,8 +25,8 @@ export const socials: Array<{ icon: ReactNode; label: string; link: string; }> =
 export function SocialLinks(): ReactNode {
     return (
         <ButtonGroup sx={{ alignItems: "center", justifyContent: "space-evenly", width: "min-content" }}>
-            {socials.map(({ icon, label, link }, i) => (
-                <Tooltip key={i} title={label} arrow>
+            {socials.map(({ icon, label, link, name }, i) => (
+                <Tooltip key={i} title={`${label}: ${name}`} arrow>
                     <IconButton component={Link} href={link} prefetch={false} sx={{ color: "inherit", padding: 0.5 }}>
                         {icon}
                     </IconButton>
