98a11a58a68746410c1587423183c96ee4b7dd16
Ollie Jones <oathompsonjones@gmail.com>
2025-05-03 23:03:49 +0100

    Layout and language improvements

--- src/app/(pages)/privacy/page.tsx
+++ src/app/(pages)/privacy/page.tsx
@@ -8,18 +8,16 @@ import { Typography } from "@mui/material";
  */
 export default function Privacy(): ReactNode {
     return (
-        <>
-            <Typography variant="h2">
-                Privacy Policy
-            </Typography>
+        <div>
+            <Typography variant="h2">Privacy Policy</Typography>
             <Typography>
-                I reserve the right to collect the names and email address of any and all users who
-                submit the contact form located <Link href="/contact">here</Link>.
+                I reserve the right to collect the names and email addresses of any and all users who submit the
+                contact form located <Link href="/contact">here</Link>.
                 <br />
                 This information will not be shared with any other parties.
                 <br />
-                This information will only be used to allow me to respond to any enquiries.
+                This information will only be used to allow me to respond to enquiries.
             </Typography>
-        </>
+        </div>
     );
 }
