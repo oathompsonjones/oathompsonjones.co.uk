30ee6375869b3049c3f3a639df5811fccbf14844
Ollie Jones <oathompsonjones@gmail.com>
2026-03-13 09:05:11 +0000

    Create Recaptcha component with legal text
    Add Recaptcha component to display legal text

---
+++ src/components/pages/privacy/recaptcha.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+import type { TypographyProps } from "@mui/material";
+import { Typography } from "@mui/material";
+
+/**
+ * Contains the legal text from Google about Recaptcha Ts and Cs.
+ * @param props - The props from the MUI Typography component.
+ * @returns The legal text from Google.
+ */
+export default function Recaptcha(props: TypographyProps): ReactNode {
+    return (
+        <Typography {...props}>
+            This site is protected by reCAPTCHA and the Google <Link href="https://policies.google.com/privacy">
+                Privacy Policy</Link> and <Link href="https://policies.google.com/terms">
+                Terms of Service</Link> apply.
+        </Typography>
+    );
+}
